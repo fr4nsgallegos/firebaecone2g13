b57a4d1be0ab614334ed93214ee35034587608d6
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2025-10-03 20:28:44 -0500

    creando modelo

---
+++ lib/models/user_model.dart
@@ -0,0 +1,15 @@
+class UserModel {
+  String name;
+  String lastName;
+  int age;
+  double weight;
+  String nacionality;
+
+  UserModel({
+    required this.name,
+    required this.lastName,
+    required this.age,
+    required this.weight,
+    required this.nacionality,
+  });
+}
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,10 +1,36 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  HomePage({super.key});
+
+  CollectionReference userReference = FirebaseFirestore.instance.collection(
+    'users',
+  );
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text('Firebase Core Initialized'),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                userReference.get().then((value) {
+                  List<QueryDocumentSnapshot> docs = value.docs;
+                  docs.forEach((doc) {
+                    print(doc.data());
+                  });
+                });
+              },
+              child: Text('Obtener información'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
