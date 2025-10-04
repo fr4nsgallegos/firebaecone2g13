2a305398886b1817cf59b4ade4c6d02db5aa0851
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2025-10-04 11:52:56 -0500

    show custom info window

--- lib/pages/maps/map2_page.dart
+++ lib/pages/maps/map2_page.dart
@@ -1,3 +1,4 @@
+import 'package:custom_info_window/custom_info_window.dart';
 import 'package:firebaecone2g13/pages/maps/model_maps.dart/place_model.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
@@ -11,6 +12,8 @@ class Map2Page extends StatefulWidget {
 
 class _Map2PageState extends State<Map2Page> {
   Set<Marker> markers = {};
+  CustomInfoWindowController _customInfoWindowController =
+      CustomInfoWindowController();
 
   Future<void> addMarkers() async {
     Set<Marker> auxMarkers = Set();
@@ -21,14 +24,42 @@ class _Map2PageState extends State<Map2Page> {
     );
 
     // MÉTODO 1
-    auxMarkers = places.map((place) {
-      return Marker(
-        markerId: MarkerId(place.id.toString()),
-        position: place.position,
-        icon: iconMarker,
-        infoWindow: InfoWindow(title: place.name, snippet: place.services),
+    // auxMarkers = places.map((place) {
+    //   return Marker(
+    //     markerId: MarkerId(place.id.toString()),
+    //     position: place.position,
+    //     icon: iconMarker,
+    //     infoWindow: InfoWindow(title: place.name, snippet: place.services),
+    //   );
+    // }).toSet();
+
+    // METODO 2
+    places.forEach((place) {
+      auxMarkers.add(
+        Marker(
+          markerId: MarkerId(place.id.toString()),
+          position: place.position,
+          icon: iconMarker,
+          // infoWindow: InfoWindow(title: place.name, snippet: place.services),
+          onTap: () {
+            _customInfoWindowController.addInfoWindow!(
+              Container(
+                width: 250,
+                height: 150,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  border: Border.all(color: Colors.blueAccent),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Text("Hola"),
+              ),
+              place.position,
+            );
+          },
+        ),
       );
-    }).toSet();
+    });
+
     markers = auxMarkers;
     print(markers.length);
     setState(() {});
@@ -43,12 +74,31 @@ class _Map2PageState extends State<Map2Page> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: GoogleMap(
-        initialCameraPosition: CameraPosition(
-          target: LatLng(-12.046374, -77.042793),
-          zoom: 12,
-        ),
-        markers: markers,
+      body: Stack(
+        children: [
+          GoogleMap(
+            onMapCreated: (GoogleMapController controller) async {
+              _customInfoWindowController.googleMapController = controller;
+            },
+            initialCameraPosition: CameraPosition(
+              target: LatLng(-12.046374, -77.042793),
+              zoom: 12,
+            ),
+            markers: markers,
+            onTap: (LatLng position) {
+              _customInfoWindowController.hideInfoWindow!();
+            },
+            onCameraMove: (position) {
+              _customInfoWindowController.onCameraMove!();
+            },
+          ),
+          CustomInfoWindow(
+            controller: _customInfoWindowController,
+            height: 150,
+            width: 250,
+            offset: 50,
+          ),
+        ],
       ),
     );
   }
